97e12629151bb6c6480ead8350cee7f4a22737fd
olhabahaieva <bahaieva.olha@gmail.com>
2023-04-20 12:24:11 +0000

    Deploying to gh-pages from @ olhabahaieva/goit-js-hw-10@c06675c701b6c69baa1ea3f30d84600edc228e77 🚀

---
+++ index.032e1db3.js
@@ -0,0 +1,2 @@
+document.querySelector("input[type = text]"),document.querySelector(".country-list");
+//# sourceMappingURL=index.032e1db3.js.map
---
+++ index.705bf832.js
@@ -0,0 +1,2 @@
+document.querySelector("input[type = text]"),document.querySelector(".country-list");
+//# sourceMappingURL=index.705bf832.js.map
