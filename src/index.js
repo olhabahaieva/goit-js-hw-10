c06675c701b6c69baa1ea3f30d84600edc228e77
Olha Bahaieva <bahaieva.olha@gmail.com>
2023-04-20 15:23:50 +0300

    created new js file and included selectors for the major js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+// Default JS ends
+
+//Input selector
+const inputElement = document.querySelector('input[type = text]');
+const countryListElement = document.querySelector('.country-list');
