b51391012d3e57b613c5bc4bb2057d2883c2e295
olhabahaieva <bahaieva.olha@gmail.com>
2023-04-22 11:49:39 +0000

    Deploying to gh-pages from @ olhabahaieva/goit-js-hw-10@b00edcf45bc471721b6cda5a64a42729d8523892 🚀

--- index.74deea0a.js
+++
@@ -1,2 +0,0 @@
-!function(){function n(n){var t="".concat("https://restcountries.com/v3.1","/name/").concat(n,"?").concat("fields=name,capital,official,population,flags,languages");return fetch(t).then((function(n){if(!n.ok)throw new Error(n.statusText);return n.json()}))}var t=document.querySelector("input"),e=document.querySelector(".countries-list");t.addEventListener("input",(function(t){t.preventDefault(),n(t.currentTarget.value).then((function(n){return e.innerHTML=void n.name})).catch((function(n){return console.log(n)}))}))}();
-//# sourceMappingURL=index.74deea0a.js.map
